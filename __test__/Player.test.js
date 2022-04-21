c5bbc9da2d444878ae2427d9b5872402b90d26c2
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-04-21 17:44:46 -0400

    Added tests for potions and inventory

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -66,3 +66,33 @@ test("subtracts from player's health", () => {
 
   expect(player.health).toBe(0);
 });
+
+// hard to test for randomness in a range so we don't test for ANY number
+// but we test specificity to give more value and actionable feedback
+test("gets player's attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//
+test("adds a potion to the inventory", () => {
+  const player = new Player("Dave");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test("uses a potion from inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
+});
